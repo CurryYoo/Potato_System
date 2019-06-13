1a936a8ec0edc7918f6e19ba889ae661bddbe8b3
CheatGZ <zyk970512@163.com>
2019-06-13 10:04:57 +0800

    mutlilevel

---
+++ app/src/main/java/com/example/kerne/potato/MultiLevelActivity.java
@@ -0,0 +1,177 @@
+package com.example.kerne.potato;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.kerne.potato.complextable.widget.multilevellist.TreeAdapter;
+import com.example.kerne.potato.complextable.widget.multilevellist.TreePoint;
+import com.example.kerne.potato.complextable.widget.multilevellist.TreeUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+public class MultiLevelActivity extends AppCompatActivity {
+
+    private TreeAdapter adapter;
+    private ListView listView;
+    private EditText et_filter;
+    private List<TreePoint> pointList = new ArrayList<>();
+    private HashMap<String, TreePoint> pointMap = new HashMap<>();
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_mutlilevel);
+        init();
+        addListener();
+    }
+
+    public void init() {
+        adapter = new TreeAdapter(this, pointList, pointMap);
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        et_filter = findViewById(R.id.et_filter);
+        initData();
+    }
+
+    //初始化数据
+    //数据特点：TreePoint 之间的关系特点   id是任意唯一的。    如果为根节点 PARENTID  为"0"   如果没有子节点，也就是本身是叶子节点的时候ISLEAF = "1"
+    //  DISPLAY_ORDER 是同一级中 显示的顺序
+    //如果需要做选中 单选或者多选，只需要给TreePoint增加一个选中的属性，在ReasonAdapter中处理就好了
+    private void initData() {
+        pointList.clear();
+        int id = 1000;
+        int parentId = 0;
+        int parentId2 = 0;
+        int parentId3 = 0;
+        int parentId4 = 0;
+        for (int i = 1; i < 5; i++) {
+            id++;
+            pointList.add(new TreePoint("" + id, "分类" + i, "" + parentId, "0", i));
+            for (int j = 1; j < 3; j++) {
+                if (j == 1) {
+                    parentId2 = id;
+                }
+                id++;
+                pointList.add(new TreePoint("" + id, "分类" + i + "_" + j, "" + parentId2, "0", j));
+                for (int k = 1; k < 5; k++) {
+                    if (k == 1) {
+                        parentId3 = id;
+                    }
+                    id++;
+                    pointList.add(new TreePoint("" + id, "分类" + i + "_" + j + "_" + k, "" + parentId3, "0", k));
+                    for (int l = 1; l < 5; l++) {
+                        if (l == 1) {
+                            parentId4 = id;
+                        }
+                        id++;
+                        pointList.add(new TreePoint("" + id, "分类" + i + "_" + j + "_" + k + "_"+l, "" + parentId4, "1", l));
+                    }
+                }
+            }
+        }
+        //打乱集合中的数据
+        Collections.shuffle(pointList);
+        //对集合中的数据重新排序
+        updateData();
+    }
+
+
+    public void addListener() {
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                adapter.onItemClick(position);
+            }
+        });
+
+        et_filter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                searchAdapter(s);
+            }
+        });
+    }
+
+    private void searchAdapter(Editable s) {
+        adapter.setKeyword(s.toString());
+    }
+
+    //对数据排序 深度优先
+    private void updateData() {
+        for (TreePoint treePoint : pointList) {
+            pointMap.put(treePoint.getID(), treePoint);
+        }
+        Collections.sort(pointList, new Comparator<TreePoint>() {
+            @Override
+            public int compare(TreePoint lhs, TreePoint rhs) {
+                int llevel = TreeUtils.getLevel(lhs, pointMap);
+                int rlevel = TreeUtils.getLevel(rhs, pointMap);
+                if (llevel == rlevel) {
+                    if (lhs.getPARENTID().equals(rhs.getPARENTID())) {  //左边小
+                        return lhs.getDISPLAY_ORDER() > rhs.getDISPLAY_ORDER() ? 1 : -1;
+                    } else {  //如果父辈id不相等
+                        //同一级别，不同父辈
+                        TreePoint ltreePoint = TreeUtils.getTreePoint(lhs.getPARENTID(), pointMap);
+                        TreePoint rtreePoint = TreeUtils.getTreePoint(rhs.getPARENTID(), pointMap);
+                        return compare(ltreePoint, rtreePoint);  //父辈
+                    }
+                } else {  //不同级别
+                    if (llevel > rlevel) {   //左边级别大       左边小
+                        if (lhs.getPARENTID().equals(rhs.getID())) {
+                            return 1;
+                        } else {
+                            TreePoint lreasonTreePoint = TreeUtils.getTreePoint(lhs.getPARENTID(), pointMap);
+                            return compare(lreasonTreePoint, rhs);
+                        }
+                    } else {   //右边级别大   右边小
+                        if (rhs.getPARENTID().equals(lhs.getID())) {
+                            return -1;
+                        }
+                        TreePoint rreasonTreePoint = TreeUtils.getTreePoint(rhs.getPARENTID(), pointMap);
+                        return compare(lhs, rreasonTreePoint);
+                    }
+                }
+            }
+        });
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                break;
+//                Intent intent = new Intent(this, MainActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+            default:
+        }
+        return true;
+    }
+}
