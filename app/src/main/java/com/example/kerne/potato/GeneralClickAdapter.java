62178fff9d1e9dd84374476d404184768446e11f
CheatGZ <zyk970512@163.com>
2019-07-05 23:54:00 +0800

    合并大棚外品种规划和品种种植界面

--- app/src/main/java/com/example/kerne/potato/GeneralClickAdapter.java
+++ app/src/main/java/com/example/kerne/potato/GeneralClickAdapter.java
@@ -137,40 +137,47 @@ public class GeneralClickAdapter extends RecyclerView.Adapter<GeneralClickAdapte
                 else if (type.equals("greenhouse")) {
                     intent = new Intent(mContext, TableActivity.class);
                     final int[] status = new int[1];
-                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-                    builder.setTitle("选择一个操作");
-                    // 指定下拉列表的显示数据
-                    final String[] options = {"品种规划", "查看品种规划详情"};
-                    // 设置一个下拉的列表选择项
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+//                    builder.setTitle("选择一个操作");
+//                    // 指定下拉列表的显示数据
+//                    final String[] options = {"品种规划", "查看品种规划详情"};
+//                    // 设置一个下拉的列表选择项
                     final Intent finalIntent = intent;
-                    builder.setItems(options, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            if(which == 0){    // 点击第一个操作"品种规划"时
-//                                intent = new Intent(mContext, TableActivity.class);
-                                finalIntent.putExtra("status", STATUS_EDIT);
-                                finalIntent.putExtra("expType", expType);
-                                finalIntent.putExtra("fieldId", fieldId);
-                                finalIntent.putExtra("num", num);
-                                finalIntent.putExtra("rows", rows);
-                                finalIntent.putExtra("farmlandId", farmlandId);
-                                finalIntent.putExtra("type", type);
-                                mContext.startActivity(finalIntent);
-//                                        intent.putExtra("userRole", userRole);
-                            }
-                            else {   // 点击第二个操作"查看品种规划详情"时
-                                finalIntent.putExtra("status", STATUS_READ);
-                                finalIntent.putExtra("expType", expType);
-                                finalIntent.putExtra("fieldId", fieldId);
-                                finalIntent.putExtra("num", num);
-                                finalIntent.putExtra("rows", rows);
-                                finalIntent.putExtra("farmlandId", farmlandId);
-                                finalIntent.putExtra("type", type);
-                                mContext.startActivity(finalIntent);
-                            }
-                        }
-                    });
-                    builder.show();
+                    finalIntent.putExtra("expType", expType);
+                    finalIntent.putExtra("fieldId", fieldId);
+                    finalIntent.putExtra("num", num);
+                    finalIntent.putExtra("rows", rows);
+                    finalIntent.putExtra("farmlandId", farmlandId);
+                    finalIntent.putExtra("type", type);
+                    mContext.startActivity(finalIntent);
+//                    builder.setItems(options, new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            if(which == 0){    // 点击第一个操作"品种规划"时
+////                                intent = new Intent(mContext, TableActivity.class);
+//                                finalIntent.putExtra("status", STATUS_EDIT);
+//                                finalIntent.putExtra("expType", expType);
+//                                finalIntent.putExtra("fieldId", fieldId);
+//                                finalIntent.putExtra("num", num);
+//                                finalIntent.putExtra("rows", rows);
+//                                finalIntent.putExtra("farmlandId", farmlandId);
+//                                finalIntent.putExtra("type", type);
+//                                mContext.startActivity(finalIntent);
+////                                        intent.putExtra("userRole", userRole);
+//                            }
+//                            else {   // 点击第二个操作"查看品种规划详情"时
+//                                finalIntent.putExtra("status", STATUS_READ);
+//                                finalIntent.putExtra("expType", expType);
+//                                finalIntent.putExtra("fieldId", fieldId);
+//                                finalIntent.putExtra("num", num);
+//                                finalIntent.putExtra("rows", rows);
+//                                finalIntent.putExtra("farmlandId", farmlandId);
+//                                finalIntent.putExtra("type", type);
+//                                mContext.startActivity(finalIntent);
+//                            }
+//                        }
+//                    });
+//                    builder.show();
                 }
 
 //                //弹出输入年份的对话框
